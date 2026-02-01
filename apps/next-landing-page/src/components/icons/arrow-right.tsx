48e49e3414903f052f7dc726647503af604b1a91
bayunugrohoDev <bayunugroho963@gmail.com>
2026-02-01 09:42:53 +0700

    feat(landing): add icon components

---
+++ apps/next-landing-page/src/components/icons/arrow-right.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const ArrowRight = () => {
+  return (
+    <svg
+      width="8"
+      height="14"
+      viewBox="0 0 8 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.499995 0.5L6.65 6.65C6.69783 6.69489 6.73595 6.74911 6.76201 6.80931C6.78807 6.8695 6.80151 6.9344 6.80151 7C6.80151 7.0656 6.78807 7.1305 6.76201 7.19069C6.73595 7.25089 6.69783 7.30511 6.65 7.35L0.499995 13.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
---
+++ apps/next-landing-page/src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export * from "./arrow-left";
+export * from "./arrow-right";
+export * from "./fb";
+export * from "./google";
+export * from "./ig";
+export * from "./twitter";
+
